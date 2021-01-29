cccdb38e645b13c1bd22df3bb9776c2790358e17
Darkethorn <52019279+Darkethorn@users.noreply.github.com>
2021-01-28 18:44:10 -0700

    Search program in class

--- class programs/search.py
+++ class programs/search.py
@@ -1,48 +1,128 @@
 import math
+import random
+import time
 
 
-def seq_search(x, nums):
-    for i in range(len(nums)):
-        if nums[i] == x:
+def linear_search(lyst, target):
+    for i in range(len(lyst)):
+        if lyst[i] == target:
             return i
     return -1
 
 
-def binary_search(x, nums):
-    if len(nums) == 0:
+def binary_search(lyst, target):
+    if len(lyst) == 0:
         return False
 
-    midpoint = len(nums) // 2
+    midpoint = len(lyst) // 2
 
-    if nums[midpoint] == x:
+    if lyst[midpoint] == target:
         return True
     else:
-        if x < nums[midpoint]:
-            return binary_search(x, nums[:midpoint])
-        elif x > nums[midpoint]:
-            return binary_search(x, nums[midpoint + 1:])
+        if lyst[midpoint] < target:
+            return binary_search(lyst[:midpoint], target)
+        elif lyst[midpoint] > target:
+            return binary_search(lyst[midpoint + 1:], target)
 
 
-def jump_search(x, nums):
-    n = len(nums)
+def jump_search(lyst, target):
+    n = len(lyst)
     step = math.sqrt(n)
 
     prev = 0
-    while nums[int(min(step, n) - 1)] < x:
+    while lyst[int(min(step, n) - 1)] < target:
         prev = step
         step += math.sqrt(n)
         if prev >= n:
             pass
 
-    while nums[int(prev)] < x:
+    while lyst[int(prev)] < target:
         prev += 1
 
         if prev == min(step, n):
             pass
 
-    if nums[int(prev)] == x:
+    if lyst[int(prev)] == target:
         return prev
 
 
-myArray = [1, 2, 3, 5, 8, 13, 21, 34]
-print(jump_search(21, myArray))
+def main():
+    random.seed(time.time())
+
+    my_list = random.sample(range(1000000), k=1000)
+    my_list.sort()
+
+    time_start = time.perf_counter()
+
+    result = linear_search(my_list, my_list[0])
+
+    time_end = time.perf_counter()
+
+    print("Beginning Search = %s, took %0.8f" % (result, time_end-time_start))
+
+    time_start = time.perf_counter()
+
+    result = linear_search(my_list, my_list[len(my_list)//2])
+
+    time_end = time.perf_counter()
+
+    print("Middle Search = %s, took %0.8f" % (result, time_end - time_start))
+
+    time_start = time.perf_counter()
+
+    result = linear_search(my_list, my_list[-1])
+
+    time_end = time.perf_counter()
+
+    print("End Search = %s, took %0.8f" % (result, time_end - time_start))
+
+    time_start = time.perf_counter()
+
+    result = binary_search(my_list, my_list[0])
+
+    time_end = time.perf_counter()
+
+    print("Beginning Search = %s, took %0.8f" % (result, time_end - time_start))
+
+    time_start = time.perf_counter()
+
+    result = binary_search(my_list, my_list[len(my_list) // 2])
+
+    time_end = time.perf_counter()
+
+    print("Middle Search = %s, took %0.8f" % (result, time_end - time_start))
+
+    time_start = time.perf_counter()
+
+    result = binary_search(my_list, my_list[-1])
+
+    time_end = time.perf_counter()
+
+    print("End Search = %s, took %0.8f" % (result, time_end - time_start))
+
+    time_start = time.perf_counter()
+
+    result = jump_search(my_list, my_list[0])
+
+    time_end = time.perf_counter()
+
+    print("Beginning Search = %s, took %0.8f" % (result, time_end - time_start))
+
+    time_start = time.perf_counter()
+
+    result = jump_search(my_list, my_list[len(my_list) // 2])
+
+    time_end = time.perf_counter()
+
+    print("Middle Search = %s, took %0.8f" % (result, time_end - time_start))
+
+    time_start = time.perf_counter()
+
+    result = jump_search(my_list, my_list[-1])
+
+    time_end = time.perf_counter()
+
+    print("End Search = %s, took %0.8f" % (result, time_end - time_start))
+
+
+main()
