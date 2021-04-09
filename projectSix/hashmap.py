99be9fcb76c7c37a27472cf9d7f55f8ddefab2e0
Matt Ward <matt.ward3505@gmail.com>
2021-04-08 18:45:05 -0600

    My updates April 8th 2021

--- projectSix/hashmap.py
+++ projectSix/hashmap.py
@@ -1,10 +1,10 @@
 class HashMap:
     def __init__(self):
-        self.inner_size = 0
+        self.inner_size = 7
         self.array = [None] * self.inner_size
         self.count = 0
 
-    def isfull(self):
+    def is_full(self):
         for i in range(self.inner_size):
             if self.array[i] is None:
                 return False
@@ -13,38 +13,37 @@ class HashMap:
     def size(self):
         return self.count
 
-    def hashTuple(self, tupleValue):
-        if tupleValue is tuple:
-            return tupleValue[0] ** tupleValue[1]
-        return hash(tupleValue)
+    def hash_tuple(self, tuple_value):
+        if tuple_value is tuple:
+            return tuple_value[0] ** tuple_value[1]
+        return hash(tuple_value)
 
     def set(self, key, value):
-        index = self.hashTuple(key) % self.inner_size
+        index = self.hash_tuple(key) % self.inner_size
 
-        if self.isfull():
+        if self.is_full():
             self.inner_size = self.inner_size * 2
             temp = self.array
             self.array = [None] * self.inner_size
             for item in temp:
-                self.add(item[0], item[1])
+                self.set(item[0], item[1])
         if self.array[index] is None:
             self.array[index] = []
-            self.array[index].append((key, value))
+            self.array[index].append(key, value)
         else:
             for keyValuePair in self.array[index]:
                 if keyValuePair[0] == key:
                     keyValuePair[0] = value
                     return
-            self.array[index].append((key, value))
+            self.array[index].append(key, value)
         self.count += 1
-        if self.getLoadFactor() >= 0.80:
+        if self.get_load_factor() >= 0.80:
             self.rehash()
 
     def remove(self, key):
         pass
 
     def clear(self):
-        self.inner_size = 0
         self.array = [None] * self.inner_size
         self.count = 0
 
@@ -71,7 +70,7 @@ class HashMap:
         for keyValue in temp:
             self.set(keyValue[0], keyValue[1])
 
-    def getLoadFactor(self):
+    def get_load_factor(self):
         return self.count / self.inner_size
 
     def __hash__(self):
